42482ba115a388c01ba7d11161f13e20065d1d99
Martin Leischner <martinleischner@web.de>
2021-11-23 18:41:44 +0100

    Installed Vue + Bootstrap + First Steps

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Rezepte from '../views/Rezepte'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/rezepte',
+    name: 'Rezepte',
+    component: Rezepte
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  linkActiveClass: 'active',
+  routes
+})
+
+export default router
