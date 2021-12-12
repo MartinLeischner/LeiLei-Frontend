f4a32a3a8d612783b2c3f2dfaac0a762e668efcb
Martin Leischner <martinleischner@web.de>
2021-12-12 11:51:02 +0100

    Added Recipe Card

--- src/views/Rezepte.vue
+++ src/views/Rezepte.vue
@@ -1,10 +1,61 @@
 <template>
 <h1>Hier finden Sie unsere Rezepte</h1>
+  <div class="col" v-for="rezept in rezepte" :key="rezept.id">
+    <div class="card" style="width: 18rem;">
+     <img src="../assets/kisspng-cheese-sandwich-butterbrot-bread-bread-cheese-sandwich-5a9d7293bf4f09.1927332815202679237836.jpg" class="card-img-top" :alt="rezept.name">
+      <div class="card-body">
+       <h5 class="card-title">{{rezept.name}}</h5>
+       <p class="card-text">{{rezept.ingredient}}</p>
+     </div>
+     <ul class="list-group list-group-flush">
+       <li class="list-group-item">{{rezept.time}}</li>
+       <li class="list-group-item">{{rezept.diifculty}}</li>
+       <li class="list-group-item">A third item</li>
+     </ul>
+     <div class="card-body">
+       <a href="#" class="card-link">Card link</a>
+       <a href="#" class="card-link">Another link</a>
+     </div>
+   </div>
+
+  </div>
+<!--  <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">An item</li>
+      <li class="list-group-item">A second item</li>
+      <li class="list-group-item">A third item</li>
+    </ul>
+    <div class="card-body">
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+  </div>
+  -->
 </template>
 
 <script>
 export default {
-  name: 'Rezepte'
+  name: 'Rezepte',
+  data() {
+    return {
+      rezepte: [
+        {
+          id: "1",
+          name: 'Kaesesandwich',
+          ingredient: 'Brot, Käse, Mayo, Salami, Salat',
+          diifculty: 'leicht',
+          time: '10'
+        }
+      ]
+    }
+  }/*,
+  methods: {
+  }*/
 }
 </script>
 
