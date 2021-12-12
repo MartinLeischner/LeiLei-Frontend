3513109810c35c6847d89d047ae58762c4854c76
Martin Leischner <martinleischner@web.de>
2021-12-12 13:47:36 +0100

    Bugfixes

--- src/views/Rezepte.vue
+++ src/views/Rezepte.vue
@@ -47,7 +47,7 @@ export default {
     }
   },
   mounted () {
-    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + 'api/v1/rezepte'
+    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/rezepte'
     const requestOptions = {
       method: 'GET',
       redirect: 'follow'
